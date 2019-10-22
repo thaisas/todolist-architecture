4f5aab64789a9b5a56307ce0dcf373a7cc7d0f99
Bruno Salgado <brunobetabio@gmail.com>
2019-10-22 20:49:11 -0300

    axios module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 
 export default {
   mode: 'spa',
@@ -33,6 +34,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxtjs/dotenv',
   ],
   /*
   ** Nuxt.js modules
